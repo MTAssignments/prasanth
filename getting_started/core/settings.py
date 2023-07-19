c9baf10133084625bfa3709cc3c3bbbdab0e907a
Amar Kanth BOLLU <amarbo.in@mouritech.com>
2023-07-19 10:34:56 +0530

    renamed project app to core

--- getting_started/core/settings.py
+++ getting_started/core/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'project333.urls'
+ROOT_URLCONF = 'core.urls'
 
 TEMPLATES = [
     {
@@ -68,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'project333.wsgi.application'
+WSGI_APPLICATION = 'core.wsgi.application'
 
 
 # Database
